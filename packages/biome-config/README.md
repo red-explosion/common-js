728e9da23f45282b5ca1d5bad4f4c5f798324fc6
Ben Sherred <ben@sherred.co.uk>
2023-12-26 18:07:44 +0000

    feat: add biome config package

---
+++ packages/biome-config/README.md
@@ -0,0 +1,29 @@
+<p align="center"><img width="400" src="https://raw.githubusercontent.com/red-explosion/common-js/main/art/logo.svg" alt="Red Explosion Logo"></p>
+
+# @red-explosion/biome-config
+
+This is where your description should go. Limit it to a paragraph or two. Consider adding a small example.
+
+## Usage
+
+Add the config to your project dependencies:
+
+```bash 
+yarn add -D @red-explosion/biome-config
+
+or
+
+pnpm add -D @red-explosion/biome-config
+
+or
+
+npm install -D @red-explosion/biome-config
+```
+
+## Contributing
+
+Please see [CONTRIBUTING](../../CONTRIBUTING.md) for details.
+
+## License
+
+@red-explosion/biome-config is open-sourced software licensed under the [MIT license](LICENSE.md).
--- scripts/make-package/index.js
+++ scripts/make-package/index.js
@@ -1,57 +1,70 @@
-import { writeFileSync, readFileSync, existsSync, rmdirSync, cpSync } from 'node:fs';
-import { resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
-import pkg from '../../package.json' assert { type: 'json' };
+import {
+  cpSync,
+  existsSync,
+  readFileSync,
+  rmdirSync,
+  writeFileSync,
+} from "node:fs";
+import { resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import pkg from "../../package.json" assert { type: "json" };
 
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 const packageName = process.argv[2];
 
-const root = resolve(__dirname, '../../');
+const root = resolve(__dirname, "../../");
 
 if (!packageName) {
-    console.error('Please provide a package name, i.e. `pnpm make:package my-package`');
+  console.error(
+    "Please provide a package name, i.e. `pnpm make:package my-package`",
+  );
 
-    process.exit();
+  process.exit();
 }
 
-const packageDirectory = resolve(root, './packages', packageName);
+const packageDirectory = resolve(root, "./packages", packageName);
 
 if (existsSync(packageDirectory)) {
-    console.error('Package already exists');
+  console.error("Package already exists");
 
-    process.exit();
+  process.exit();
 }
 
 try {
-    cpSync(resolve(__dirname, './stubs'), packageDirectory, {
-        recursive: true,
-    });
+  cpSync(resolve(__dirname, "./stubs"), packageDirectory, {
+    recursive: true,
+  });
 
-    const packageJson = JSON.parse(readFileSync(resolve(packageDirectory, './package.json'), 'utf-8'));
+  const packageJson = JSON.parse(
+    readFileSync(resolve(packageDirectory, "./package.json"), "utf-8"),
+  );
 
-    packageJson.version = pkg.version;
-    packageJson.name = `@red-explosion/${ packageName }`;
-    packageJson.homepage = `https://github.com/red-explosion/common-js/tree/main/packages/${ packageName }`;
+  packageJson.version = pkg.version;
+  packageJson.name = `@red-explosion/${packageName}`;
+  packageJson.homepage = `https://github.com/red-explosion/common-js/tree/main/packages/${packageName}`;
 
-    writeFileSync(
-        resolve(packageDirectory, 'package.json'),
-        JSON.stringify(packageJson, null, 2),
-        'utf-8',
-    );
+  writeFileSync(
+    resolve(packageDirectory, "package.json"),
+    JSON.stringify(packageJson, null, 2),
+    "utf-8",
+  );
 
-    const readmeContents = readFileSync(resolve(packageDirectory, './README.md'), 'utf-8');
+  const readmeContents = readFileSync(
+    resolve(packageDirectory, "./README.md"),
+    "utf-8",
+  );
 
-    writeFileSync(
-        resolve(packageDirectory, 'README.md'),
-        readmeContents.replace(/{{PACKAGE_NAME}}/g, packageName),
-        'utf-8',
-    );
+  writeFileSync(
+    resolve(packageDirectory, "README.md"),
+    readmeContents.replace(/{{PACKAGE_NAME}}/g, packageName),
+    "utf-8",
+  );
 
-    console.log(`✅ @red-explosion/${ packageName } bootstrapped successfully`);
+  console.log(`✅ @red-explosion/${packageName} bootstrapped successfully`);
 } catch (e) {
-    rmdirSync(packageDirectory, {
-        recursive: true,
-    });
+  rmdirSync(packageDirectory, {
+    recursive: true,
+  });
 
-    throw e;
+  throw e;
 }
